bb42070b4b8b9bd71d48363f2bc79b2e0fe68653
Daniil Bombenkov <danixoon@gmail.com>
2021-11-12 08:13:54 +0000

    deploy comments & fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+# Установка зависимостей
 yarn backend:build
-mkdir ./build/public
+cd client && yarn build && cd ..
 
-cd client && yarn build
-mv ./build/* ../build/public
-cd ..
+# Перемещение билда фронта
+mkdir ./client/build/public
+mv ./client/build/* ./build/public
 
+# Проверка на -z флаг создания .zip архива прода
 zip=false
 while getopts 'z' flag
 do
@@ -15,19 +17,22 @@ do
   esac
 done
 
-rm -rf deploy
-mkdir deploy
-
+# Перемещение кода билда и зависимостей в директорию прода
+rm -rf ./deploy || mkdir ./deploy
 cp -r ./node_modules ./deploy
 mv ./build ./deploy
 
-echo "SET NODE_ENV=production node ./build/index.js" > ./deploy/start.bat
-echo -e '#!/usr/bin/env bash\nNODE_ENV=production node ./build/index.js' > ./deploy/start.sh
-echo -e 'process.env.NODE_ENV="production";\n' >> ./deploy/build/index.js
+# Установка флага NODE_ENV на прод
+echo -e 'process.env.NODE_ENV="production";\n' | cat - ./deploy/build/index.js > temp && mv temp ./deploy/build/index.js
+# Создание сценарных файлов запуска для винды/линухи
+echo "node ./build/index.js" > ./deploy/start.bat
+echo -e '#!/usr/bin/env bash\n node ./build/index.js' > ./deploy/start.sh
 
+# Копирование продового шаблона env-переменных и файла списка зависимостей
 cp .production.env ./deploy
 cp package.json ./deploy
 
+# Создание .zip архива прода
 if $zip; then
   if [[ -e "./deploy.zip" ]]; then
     rm ./deploy.zip
