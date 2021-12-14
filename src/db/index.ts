bfb09cac07d47736739208e908cdba74ca3a2dcc
Dan <daniy.b@mail.ru>
2021-12-14 06:22:32 +0000

    deploy fixes

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import { promises as fs, existsSync } from "fs";
+import minimist from "minimist";
 import { fillDevDatabase, fillProdDatabase } from "@backend/utils/db";
 import { Sequelize } from "sequelize-typescript";
 import { logger } from "@backend/utils/index";
@@ -7,7 +8,64 @@ import { logger } from "@backend/utils/index";
 export let sequelize: Sequelize;
 let dbLogger: fs.FileHandle;
 
+type SyncConfig = {
+  date: Date;
+  error: null | any;
+};
+
 export const getModel = (name: string) => sequelize.models[name];
+
+const configFilename = path.resolve(__dirname, "../../sync.json");
+export const getSyncConfig = async () => {
+  if (!existsSync(configFilename)) return null;
+  try {
+    const file = await fs.readFile(configFilename, { encoding: "utf8" });
+    const sync = JSON.parse(file);
+    const config: SyncConfig = {
+      date: new Date(sync.date),
+      error: sync.error || null,
+    };
+    return config;
+  } catch (err) {
+    throw new Error(
+      "Некорректная конфигурация sync.json. Попробуйте удалить его и перезапустить сервер."
+    );
+  }
+};
+
+export const dropDatabase = () => {
+  logger.info(`База данных очищается`, {
+    service: "database",
+  });
+  return sequelize.drop({});
+};
+export const syncDatabase = async (force: boolean) => {
+  logger.info(`База данных синхронизируется`, {
+    service: "database",
+  });
+  const now = new Date();
+  const config: SyncConfig = {
+    date: now,
+    error: null,
+  };
+  try {
+    await sequelize.sync({
+      logging: (sql) => {
+        const t = new Date();
+        const log = `[${t.toLocaleDateString()} ${t.toLocaleTimeString()} | sync] ${sql}\n`;
+        dbLogger.write(log);
+      },
+      force,
+    });
+    return config;
+  } catch (err) {
+    config.error = err;
+    throw err;
+  } finally {
+    const json = JSON.stringify(config);
+    await fs.writeFile(configFilename, json);
+  }
+};
 
 export const init = async () => {
   // console.log(process.env);
@@ -61,18 +119,22 @@ export const init = async () => {
   const isTest = process.env.NODE_ENV === "test";
   const isFill = process.env.DEV_DB_FILL === "true";
   const isDrop = process.env.DEV_DB_DROP === "true";
+  const { sync, force } = minimist(process.argv.slice(2));
 
   if (!isTest) {
-    if (!isProd && isDrop) await sequelize.drop({});
-    await sequelize.sync({
-      logging: (sql) => {
-        const t = new Date();
-        const log = `[${t.toLocaleDateString()} ${t.toLocaleTimeString()} | sync] ${sql}\n`;
-        dbLogger.write(log);
-      },
-      force: !isProd && isFill,
-    });
+    if (sync) {
+      const config = await getSyncConfig();
+      if (!isProd && isDrop) await dropDatabase();
+      if (!config) {
+        if (force) await dropDatabase();
+        await syncDatabase(force);
+      } else
+        throw new Error(
+          "Невозможно произвести синхронизацию: она уже произведена. Удалите файл sync.json, чтобы сделать её повторно"
+        );
+    }
 
+    // 1141 <-> 1481
     if (isProd) await fillProdDatabase();
     else await fillDevDatabase(isFill, 500);
   }
